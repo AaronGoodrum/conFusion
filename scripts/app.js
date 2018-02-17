ef06a4f371a94d87a913697b63f19fe4998bbf4c
AaronGoodrum <30958265+AaronGoodrum@users.noreply.github.com>
2018-02-17 11:02:46 -0800

    app js updates

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,12 @@
-
-$(document).ready(function(){
+$(function(){
+  //   Week4 Assignment Task2
+  $('#reserve-button').click(function (){
+    $('#reserveModal').modal('toggle');
+  });
+//   Week4 Assignment Task3
+  $('#login-Btn').click(function (){
+    $('#loginModal').modal('toggle');
+  });
 
   $("#carousel-button").click(function(){
     if ($("#carousel-button").children("span").hasClass('fa-pause')) 
@@ -14,12 +21,29 @@ $(document).ready(function(){
         $("#carousel-button").children("span").addClass('fa-pause'); 
     }
   });
-//   Week4 Assignment Task2
-  $('#reserve-button').click(function (){
-    $('#reserveModal').modal('toggle');
-  });
-//   Week4 Assignment Task3
-  $('#login-Btn').click(function (){
-    $('#loginModal').modal('toggle');
-  });
+
 });
+// $(document).ready(function(){
+
+//   $("#carousel-button").click(function(){
+//     if ($("#carousel-button").children("span").hasClass('fa-pause')) 
+//         {
+//         $("#mycarousel").carousel('pause');
+//         $("#carousel-button").children("span").removeClass('fa-pause');
+//         $("#carousel-button").children("span").addClass('fa-play');
+//     }
+//     else if ($("#carousel-button").children("span").hasClass('fa-play')){
+//         $("#mycarousel").carousel('cycle');
+//         $("#carousel-button").children("span").removeClass('fa-play');
+//         $("#carousel-button").children("span").addClass('fa-pause'); 
+//     }
+//   });
+// //   Week4 Assignment Task2
+//   $('#reserve-button').click(function (){
+//     $('#reserveModal').modal('toggle');
+//   });
+// //   Week4 Assignment Task3
+//   $('#login-Btn').click(function (){
+//     $('#loginModal').modal('toggle');
+//   });
+// });
